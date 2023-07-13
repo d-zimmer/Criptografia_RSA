1cbcfc5ff11d9a845aa4e141c5f0e35fc058cdd0
David Zimmermann <davidzimmermannneto@gmail.com>
2023-07-12 21:55:04 -0300

    Update Final

--- Decripta.py
+++ Decripta.py
@@ -1,45 +1,48 @@
 import sys
-import json
+import base64
 from typing import List
 from text_chunk import TextChunk, block_size
 
-def decrypt_rsa(encrypted_data: str, key: int) -> str:
+def decrypt_rsa(encrypted_text: str, key: int, module) -> str:
     decrypted_text = ""
-    encrypted_blocks = encrypted_data.strip().split('\n')
+    encrypted_chunks = encrypted_text.strip().split('\n')
 
-    for encrypted_block in encrypted_blocks:
-        encrypted_chunk = int(encrypted_block)
-        decrypted_chunk = pow(encrypted_chunk, key, key)
-        decrypted_text += str(decrypted_chunk)
+    for chunk in encrypted_chunks:
+        encrypted_chunk = int(chunk)
+        decrypted_chunk = pow(encrypted_chunk, key, module)
+        decrypted_text += str(TextChunk(decrypted_chunk))
 
+    decrypted_text = base64.b64decode(bytes(decrypted_text,"utf-8")).decode('utf-8', "ignore")
     return decrypted_text
 
 def read_key_file(filename: str) -> int:
     with open(filename, 'r') as file:
-        key_data = json.load(file)
-    return key_data['n']
+        key_data = file.read().split('\n')
+        n = int(key_data[0])
+        d = int(key_data[1])
+    return n, d
 
 def read_data_file(filename: str) -> str:
-    with open(filename, 'r') as file:
+    with open(filename, 'r', encoding='utf-8') as file:
         data = file.read()
     return data
 
 def write_output_file(filename: str, data: str) -> None:
-    with open(filename, 'w') as file:
+    with open(filename, 'w', encoding='utf-8') as file:
         file.write(data)
 
 def main(argv: List[str]) -> None:
     if len(argv) != 3:
-        print("Usage: python decripta.py private_key.txt encrypted_texto.txt decrypted_texto.txt")
+        print("Usage: python decripta.py private_key.txt encrypt_texto.txt decrypted_texto.txt")
         return
     
     key_file = argv[0]
     input_file = argv[1]
     output_file = argv[2]
     
-    key = read_key_file(key_file)
+    n, d = read_key_file(key_file)
     encrypted_data = read_data_file(input_file)
-    decrypted_data = decrypt_rsa(encrypted_data, key)
+    decrypted_data = decrypt_rsa(encrypted_data, d, n)
     write_output_file(output_file, decrypted_data)
 
 if __name__ == '__main__':
--- Encripta.py
+++ Encripta.py
@@ -1,49 +1,50 @@
 import sys
-import json
+import base64
 from typing import List
 from text_chunk import TextChunk, block_size
 
-def encrypt_rsa(text: str, key: int) -> str:
+def encrypt_rsa(text: str, key: int, module) -> str:
     encrypted_text = ""
-    chunk_size = block_size(key)
-
-    for i in range(0, len(text), chunk_size):
-        chunk = text[i:i+chunk_size]
-        chunk_values = [ord(c) for c in chunk]
-        encrypted_chunk = [pow(val, key, key) for val in chunk_values]
-        encrypted_text += "\n".join(str(val) for val in encrypted_chunk)
-        encrypted_text += "\n"
+    chunk_size = block_size(module)
+    encoded_chunk = base64.b64encode(bytes(text, 'utf-8'))
+    
+    for i in range(0, len(encoded_chunk), chunk_size):
+        chunk = encoded_chunk[i:i+chunk_size]    
+        text_chunk = TextChunk(chunk.decode('utf-8'))
+        encrypted_chunk = pow(text_chunk.int_value(), key, module)
+        encrypted_text += str(encrypted_chunk) + "\n"
 
     return encrypted_text.strip()
 
 def read_key_file(filename: str) -> int:
     with open(filename, 'r') as file:
-        key_data = json.load(file)
-    return key_data['n']
+        key_data = file.readlines()
+        n = int(key_data[0].strip())
+        e = int(key_data[1].strip())
+    return n, e
 
 def read_data_file(filename: str) -> str:
-    with open(filename, 'r') as file:
+    with open(filename, 'r', encoding='utf-8') as file:
         data = file.read()
     return data
 
 def write_output_file(filename: str, data: str) -> None:
-    with open(filename, 'w') as file:
-        file.write(data.strip())
-        file.write('\n')
+    with open(filename, 'w', encoding='utf-8') as file:
+        file.write(data)
 
 def main(argv: List[str]) -> None:
     if len(argv) != 3:
-        print("Usage: python Encripta.py .\public_key.txt .\texto.txt encrypt_texto.txt")
+        print("Usage: python encripta.py public_key.txt texto.txt encrypt_texto")
         return
     
     key_file = argv[0]
     input_file = argv[1]
     output_file = argv[2]
     
-    key = read_key_file(key_file)
+    module, key = read_key_file(key_file)
     data = read_data_file(input_file)
-    encrypted_data = encrypt_rsa(data, key)
-    write_output_file(output_file, encrypted_data)
+    encrypted_data = encrypt_rsa(data, key, module)
+    write_output_file(output_file + ".txt", encrypted_data)
 
 if __name__ == '__main__':
     main(sys.argv[1:])
